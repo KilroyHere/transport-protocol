5edbabdbffd351771fca1f42a7bbc260e30d0089
Aryan Patel <aroo2001@gmail.com>
2022-02-26 18:29:50 -0800

    Print Packets

--- server.hpp
+++ server.hpp
@@ -10,9 +10,7 @@
 #include "constants.h"
 class TCPPacket;
 
-
-
-struct TCB 
+struct TCB
 {
 	TCB(int expectedSeqNum, int fileDescriptor, connectionState state, bool syn, struct sockaddr *cInfo, socklen_t cInfoLen)
 	{
@@ -20,45 +18,46 @@ struct TCB
 		connectionServerSeqNum = INIT_SERVER_SEQ_NUM;
 		connectionExpectedSeqNum = expectedSeqNum;
 		connectionState = state;
-        clientInfo = cInfo;
-        clientInfoLen = cInfoLen;
-        finPacket = nullptr;
+		clientInfo = cInfo;
+		clientInfoLen = cInfoLen;
+		finPacket = nullptr;
 	}
-    
-    ~TCB()
-    {
-        delete finPacket;
-        finPacket = nullptr;
-    }
 
-	std::vector<char> connectionBuffer;			   // Connection's payload buffer for each packet received
-	std::bitset<RWND_BYTES> connectionBitvector;   // A bit vector to keep track of which packets have arrived to later flush to output file
-	int connectionExpectedSeqNum;				   // Next expected Seq Number from client
-	int connectionServerSeqNum;					   // Seq number to be sent in server ack packet
-	int connectionFileDescriptor;				   // Output target file
-	c_time connectionTimer;						   // connection timer at server side (Connection closes if this runs out)
-	connectionState connectionState;			   // connection state 
+	~TCB()
+	{
+		delete finPacket;
+		finPacket = nullptr;
+	}
+
+	std::vector<char> connectionBuffer;					 // Connection's payload buffer for each packet received
+	std::bitset<RWND_BYTES> connectionBitvector; // A bit vector to keep track of which packets have arrived to later flush to output file
+	int connectionExpectedSeqNum;								 // Next expected Seq Number from client
+	int connectionServerSeqNum;									 // Seq number to be sent in server ack packet
+	int connectionFileDescriptor;								 // Output target file
+	c_time connectionTimer;											 // connection timer at server side (Connection closes if this runs out)
+	connectionState connectionState;						 // connection state
 	TCPPacket *finPacket;
-    struct sockaddr *clientInfo;
-    socklen_t clientInfoLen;
+	struct sockaddr *clientInfo;
+	socklen_t clientInfoLen;
 };
 
 class Server
 {
 public:
 	// #1
-	Server(char* port, std::string saveFolder);
+	Server(char *port, std::string saveFolder);
 	~Server();	// closes the socket
 	void run(); // engine function of the server //#3
 	int outputToStdout(std::string message);
 	int outputToStderr(std::string message);
-	int writeToFile(int connId, char *message, int len); 
+	void printPacket(TCPPacket *p, bool recvd, bool dropped, bool dup);
+	int writeToFile(int connId, char *message, int len);
 	int sendPacket(sockaddr *clientInfo, int clientInfoLen, TCPPacket *p);
 
 	// #2
 	void addNewConnection(TCPPacket *p, sockaddr *clientInfo, socklen_t clientInfoLen);
 	void setTimer(int connId);
-	bool checkTimer(int connId, float timerLimit); //false if timer runs out, true if still valid
+	bool checkTimer(int connId, float timerLimit); // false if timer runs out, true if still valid
 	bool handleFin(TCPPacket *p);
 	void closeConnection(int connId); // also will remove the connection ID entry from hashmap
 	void handleConnection();
@@ -66,13 +65,12 @@ public:
 	int flushBuffer(int connId);
 
 private:
-
 	int m_sockFd;
 	int m_nextAvailableConnectionId = 1;
 	void closeTimedOutConnectionsAndRetransmitFIN();
 	void moveWindow(int connId, int bytes);
 	std::string m_folderName;
-	std::unordered_map<int, TCB*> m_connectionIdToTCB;
+	std::unordered_map<int, TCB *> m_connectionIdToTCB;
 };
 
-#endif //SERVER_HPP
+#endif // SERVER_HPP
