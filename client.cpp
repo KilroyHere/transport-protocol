038a1230db687d030ae7ca8873eb0fe763c660ea
Sanchit Agarwal <sanchit1591@g.ucla.edu>
2022-03-03 23:01:12 -0800

    sanchit client features implemented

--- client.cpp
+++ client.cpp
@@ -55,7 +55,6 @@ Client::Client(std::string hostname, std::string port, std::string fileName)
   servInfo = NULL;
 }
 
-
 /**
  * @brief Changes the values of CWND. Should be called when 1 ACK is received. 
  * Should also be careful about duplicate ACKs to ensure they do not affect congestionControl
@@ -65,19 +64,19 @@ Client::Client(std::string hostname, std::string port, std::string fileName)
 int Client::congestionControl()
 {
   // one ACK received. Update the congestion control
-/*
+  /*
   (Slow start)            If CWND < SS-THRESH: CWND += 512
   (Congestion Avoidance)  If CWND >= SS-THRESH: CWND += (512 * 512) / CWND
 */
   int newCwnd = m_cwnd;
-  if (m_cwnd < m_ssthresh) 
+  if (m_cwnd < m_ssthresh)
   {
     newCwnd += MAX_PAYLOAD_LENGTH;
   }
-  else 
+  else
   {
     // integer division is the same as floor division for positive values
-    newCwnd = (MAX_PAYLOAD_LENGTH * MAX_PAYLOAD_LENGTH) / m_cwnd; 
+    newCwnd = (MAX_PAYLOAD_LENGTH * MAX_PAYLOAD_LENGTH) / m_cwnd;
   }
   int diff = newCwnd - m_cwnd;
   m_cwnd = newCwnd;
@@ -96,15 +95,16 @@ int Client::shiftWindow()
   int shiftedIndices = 0;
   int shiftedBytes = 0;
 
-  for (int i=0; i<m_packetBuffer.size(); i++)
+  for (int i = 0; i < m_packetBuffer.size(); i++)
   {
-    if (! m_packetACK[i]) break;
+    if (!m_packetACK[i])
+      break;
     shiftedIndices++;
     shiftedBytes += m_packetBuffer[i]->getPayloadLength();
   }
 
   // shift the values ahead
-  for (int i=shiftedIndices; i < m_packetBuffer.size(); i++)
+  for (int i = shiftedIndices; i < m_packetBuffer.size(); i++)
   {
     delete m_packetBuffer[i - shiftedIndices];
     m_packetBuffer[i - shiftedIndices] = nullptr;
@@ -115,11 +115,11 @@ int Client::shiftWindow()
   }
 
   // initialize the values at the end
-  for (int i= m_packetBuffer.size() - shiftedIndices; i < m_packetBuffer.size(); i++)
+  for (int i = m_packetBuffer.size() - shiftedIndices; i < m_packetBuffer.size(); i++)
   {
-    m_packetBuffer[i] = nullptr; 
+    m_packetBuffer[i] = nullptr;
     m_packetACK[i] = false;
-    // nothing to do for the timers 
+    // nothing to do for the timers
     m_sentOnce[i] = false;
   }
 
@@ -129,10 +129,10 @@ int Client::shiftWindow()
   return shiftedBytes;
 }
 
-void Client::markAck(TCPPacket* p)
+void Client::markAck(TCPPacket *p)
 {
   using namespace std;
-  if (p == nullptr) 
+  if (p == nullptr)
   {
     cerr << "Unexpected nullptr found in Client::markAck" << endl;
     return;
@@ -142,7 +142,7 @@ void Client::markAck(TCPPacket* p)
   // currently no implementation of what to do when ACK is beyond the window
   // since it is not clear which function to bring that into
 
-  for (int i=0; i<m_packetBuffer.size(); i++)
+  for (int i = 0; i < m_packetBuffer.size(); i++)
   {
     if (ack < m_packetBuffer[i]->getSeqNum())
       break;
@@ -157,18 +157,330 @@ void Client::markAck(TCPPacket* p)
  * @return Pointer to TCPPacket struct which the caller must delete to free the memory. If 
  * If socket is empty, return nullptr.
  */
-TCPPacket* Client::recvPacket()
+TCPPacket *Client::recvPacket()
 {
   char buffer[MAX_PACKET_LENGTH];
 
   int bytes = recvfrom(m_sockFd, buffer, MAX_PACKET_LENGTH, 0, NULL, 0); // Already have the address info of the server
 
   // nothing was available to read at the socket, so no new packet arrived
-  if (bytes == -1) return nullptr;
+  if (bytes == -1)
+    return nullptr;
 
   std::string stringBuffer = convertCStringtoStandardString(buffer, MAX_PACKET_LENGTH);
-  TCPPacket* p = new TCPPacket(stringBuffer);
+  TCPPacket *p = new TCPPacket(stringBuffer);
   return p;
+}
+
+bool Client::verifySynAck(TCPPacket *synAckPacket)
+{
+  return synAckPacket->getSeqNum() == INIT_SERVER_SEQ_NUM &&
+         synAckPacket->getAckNum() == INIT_CLIENT_SEQ_NUM + 1 &&
+         synAckPacket->isACK() == true &&
+         synAckPacket->isSYN() == true &&
+         synAckPacket->isFIN() == false;
+}
+
+bool Client::verifyFinAck(TCPPacket *finAckPacket)
+{
+  return finAckPacket->getSeqNum() == m_ackNumber &&
+         finAckPacket->getAckNum() == m_sequenceNumber &&
+         finAckPacket->isACK() == true &&
+         finAckPacket->isSYN() == false;
+  // not verifing for fin as there are 2 cases
+}
+
+/**
+ * @brief This performs a 2 way handshake (i.e Client sends a syn packet to the server and server sends
+ * back a syn-ack packet). The Client will then set the ack flag on for the first packet sent with payload
+ * to the server. This function is blocking and will not return until connection is established. 
+ * 
+ * It will set a timer for 10s and if no reply comes from the server it will also call closeConnection()
+ * 
+ * If this function returns then this means that a connection is established with the client. 
+ */
+void Client::handshake()
+{
+  // Create the SYN packet
+  TCPPacket *synPacket = new TCPPacket(
+      m_sequenceNumber, // sequence number
+      m_ackNumber,      // ack number
+      m_connectionId,   // connection id
+      false,            // is not an ACK
+      true,             // is SYN
+      false,            // is not FIN
+      0,                // no payload
+      "");
+
+  // send the packet to server
+  sendPacket(synPacket);
+  m_sequenceNumber = (m_sequenceNumber + 1) % (MAX_SEQ_NUM + 1); // as syn is 1 byte
+
+  setTimer(CONNECTION_TIMER); // set the connection timer as the first packet
+  setTimer(SYN_PACKET_TIMER); // set the syn packet timer
+
+  TCPPacket *synAckPacket;
+  while (true)
+  {
+    synAckPacket = recvPacket(); // recv the synAck packet
+
+    // if packet received and is in good form then break from loop
+    if (synAckPacket != nullptr && verifySynAck(synAckPacket))
+    {
+      // reset connection timer as packet received
+      setTimer(CONNECTION_TIMER);
+      break; // received valid syn-ack packet -> now process it
+    }
+
+    // if connection timeout -> close connection
+    if (!checkTimer(CONNECTION_TIMER, CONNECTION_TIMEOUT))
+    {
+      // delete syn and synAck ptrs
+      // NOTE: synAck doesn't need to be deleted except in the rare case
+      // when I receive an ill formed synAck packet
+      delete synPacket;
+      synPacket = nullptr;
+      delete synAckPacket;
+      synAckPacket = nullptr;
+
+      closeConnection();
+      return;
+    }
+
+    // if connection timeout -> close connection
+    if (!checkTimer(SYN_PACKET_TIMER, RETRANSMISSION_TIMER))
+    {
+      // send packets and reset timers
+      sendPacket(synPacket);
+      setTimer(SYN_PACKET_TIMER);
+    }
+  }
+
+  /* 
+    if we reach here then this means that client has received
+    the syn ack packet in valid form
+
+    this implies that the work of handshake is done and now the 
+    first packet can be sent with payload.
+  */
+
+  // set connection Id and ack no.
+  m_ackNumber = (synAckPacket->getSeqNum() + 1) % (MAX_SEQ_NUM + 1); // +1 as SYN-ACK packet is 1 byte
+  m_connectionId = synAckPacket->getConnId();
+
+  // delete syn and syn-ack packets
+  delete synPacket;
+  synPacket = nullptr;
+  delete synAckPacket;
+  synAckPacket = nullptr;
+}
+
+/**
+ * @brief This performs a 4 way handwave at the end to gracefully
+ * close the connection. This function is called when the entire 
+ * file is read. 
+ * 
+ * this will be called in run
+ */
+void Client::handwave()
+{
+  // send a fin packet
+  // NOTE: I am assuming that m_sequence here is the next available
+  // sequence number that I can send in the fin packet (so I won't
+  // increment it by 1)
+  // Create the FIN packet
+  TCPPacket *finPacket = new TCPPacket(
+      m_sequenceNumber, // sequence number
+      0,                // ack number is 0 as ack flag is not set
+      m_connectionId,   // connection id
+      false,            // is not an ACK
+      false,            // is not SYN
+      true,             // is FIN
+      0,                // no payload
+      "");
+
+  m_sequenceNumber = (m_sequenceNumber + 1) % (MAX_SEQ_NUM + 1);
+
+  // send the packet to server
+  sendPacket(finPacket);
+  setTimer(FIN_PACKET_TIMER); // set the fin packet timer
+
+  // NOTE: We need to handle both the cases when server
+  // just sends ack or sends fin-ack combined
+  TCPPacket *ackPacket;
+  while (true)
+  {
+    ackPacket = recvPacket(); // recv the ack/fin-ack packet
+
+    // if packet received and is in good form then break from loop
+    if (ackPacket != nullptr && verifyFinAck(ackPacket))
+    {
+      // reset connection timer as packet received
+      setTimer(CONNECTION_TIMER);
+
+      // set the 2 sec timer at the end
+      setTimer(FIN_END_TIMER);
+
+      // update seq no. and ack no.
+      m_ackNumber = (ackPacket->getSeqNum() + 1) % (MAX_SEQ_NUM + 1); // +1 as ACK packet is 1 byte
+      m_sequenceNumber = ackPacket->getAckNum();                      // set the sequence no. for the next ack packet to be sent by client (NOT NEEDED JUST ASSURANCE)
+      break;                                                          // received valid ack packet -> now process it
+    }
+
+    // if connection timeout -> close connection
+    if (!checkTimer(CONNECTION_TIMER, CONNECTION_TIMEOUT))
+    {
+      // delete syn and synAck ptrs
+      // NOTE: synAck doesn't need to be deleted except in the rare case
+      // when I receive an ill formed synAck packet
+      delete finPacket;
+      finPacket = nullptr;
+      delete ackPacket;
+      ackPacket = nullptr;
+
+      closeConnection();
+      return;
+    }
+
+    // if connection timeout -> close connection
+    if (!checkTimer(FIN_PACKET_TIMER, RETRANSMISSION_TIMER))
+    {
+      // send packets and reset timers
+      sendPacket(finPacket);
+      setTimer(FIN_PACKET_TIMER);
+    }
+  }
+
+  // if reach here then ack or fin ack recevied
+
+  // if fin not received -> receive fin
+  /*
+    On piazza it says that for every Fin recieved in the 2s, 
+    client sends an ack 
+  */
+  bool serverFinRevd = ackPacket->isFIN();
+
+  // create ACK PACKET
+  TCPPacket *clientAckPacket = new TCPPacket(
+      m_sequenceNumber, // sequence number
+      m_ackNumber,      // ack number set as this is an ack packet
+      m_connectionId,   // connection id
+      true,             // is an ACK
+      false,            // is not SYN
+      false,            // is not FIN
+      0,                // no payload
+      "");
+
+  if (serverFinRevd)
+  {
+    // send ack packet
+    sendPacket(clientAckPacket);
+    // NOTE: NO retransmission timers need to set
+    // we only retransmit if we recieve a fin
+  }
+
+  TCPPacket *serverFinPacket;
+  while (checkTimer(FIN_END_TIMER, CLIENT_CONNECTION_END_TIMER))
+  {
+    serverFinPacket = recvPacket(); // recv the fin packet
+
+    // if packet received and is in good form then break from loop
+    if (serverFinPacket != nullptr && serverFinPacket->isFIN())
+    {
+      sendPacket(clientAckPacket);
+      delete serverFinPacket;
+      serverFinPacket == nullptr;
+    }
+  }
+
+  // if we reach here close connection and free memory
+  delete finPacket;
+  finPacket = nullptr;
+  delete ackPacket;
+  ackPacket = nullptr;
+  delete clientAckPacket;
+  clientAckPacket = nullptr;
+  delete serverFinPacket;
+  serverFinPacket = nullptr;
+
+  closeConnection();
+  return;
+}
+
+/**
+ * @brief This function adds the provided packets to buffer and updates all other vectors to
+ * have the default value corresponding to every new packets added 
+ * 
+ * @param packets the vector of packets to be added 
+ */
+void Client::addToBuffers(std::vector<TCPPacket *> packets)
+{
+  for (const auto &packet : packets)
+  {
+    m_packetBuffer.push_back(packet);
+    m_packetACK.push_back(false);
+    m_packetTimers.push_back(std::chrono::system_clock::now()); // setting to now as no default value of c_time
+    m_sentOnce.push_back(false);
+  }
+}
+
+/**
+ * @brief This functions send all the packets that haven't been sent even once to the server 
+ * It utilizes the bool values in m_sentOnce buffers to send the packets that haven't been sent even once
+ * 
+ * The function then determines if a packet is a dup or not
+ */
+void Client::sendPackets()
+{
+  /* Assuming all 4 vectors are always of the same size */
+  for (int i = 0; i < m_sentOnce.size(); i++)
+  {
+    // We send the packets which are marked as false in sentOnce
+    if (!m_sentOnce[i])
+    {
+      sendPacket(m_packetBuffer[i]);                        // send packets
+      m_sentOnce[i] = true;                                 // set sentOnce to true
+      m_packetTimers[i] = std::chrono::system_clock::now(); // start timer
+
+      bool isDuplicate = isDup(m_packetBuffer[i]); // check if the packet is a duplicate packet;
+
+      // print to stdout
+    }
+  }
+}
+
+bool Client::isDup(TCPPacket *p)
+{
+  int seqNum = p->getSeqNum();
+  if (m_largestSeqNum < m_relSeqNum)
+  {
+    return (seqNum <= m_sequenceNumber) || (seqNum > m_relSeqNum);
+  }
+
+  else
+  {
+    return (m_relSeqNum < m_sequenceNumber) || (m_sequenceNumber <= m_largestSeqNum);
+  }
+}
+
+int Client::sendPacket(TCPPacket *p)
+{
+  int packetLength;
+  char *packetCString;
+  int bytesSent;
+
+  packetCString = p->getCString(packetLength);
+  if (p == nullptr)
+  {
+    return -1;
+  }
+
+  if ((bytesSent = sendto(m_sockFd, packetCString, packetLength, 0, m_serverInfo.ai_addr, m_serverInfoLen) == -1))
+  {
+    std::string errorMessage = "Packet send Error: " + std::string(strerror(errno));
+    // outputToStderr(errorMessage);
+  }
+  return bytesSent;
 }
 
 int main()
--- client.hpp
+++ client.hpp
@@ -16,49 +16,60 @@ public:
   Client(std::string hostname, std::string port, std::string fileName);
   ~Client();
   int readFromFile(int bytes); // returns number of bytes read
-  void run(); // lol
+  void run();                  // lol
   void handleConnection();
   void retransmitExpiredPackets(); // and reset the timer
-  void setTimers(); // i don't remember what this was for
-  void setTimer(int index);
-  void checkTimer(int index, int seconds); // have those many seconds elapsed?
+  void setTimers();                // i don't remember what this was for
+
+  void setTimer(TimerType type, int index = -1);
+  bool checkTimer(TimerType type, float timerLimit, int index = -1);
   int checkTimerAndCloseConnection();
   int checkTimersAndRetransmit();
-  void dropPackets(); // also works with lseek
-  void addToBuffers(std::vector<TCPPacket*> packets); // add the new packets to the buffers
-  void sendPackets(); // send the packets ONLY THAT HAVE NOT BEEN SENT BEFORE
-  TCPPacket* recvPacket();
-  std::vector<TCPPacket*> readAndCreateTCPPackets();// -> return vector<TCPPacket*> of the new packets created
+  void dropPackets();                                  // also works with lseek
+  void addToBuffers(std::vector<TCPPacket *> packets); // add the new packets to the buffers
+  void sendPackets();                                  // send the packets ONLY THAT HAVE NOT BEEN SENT BEFORE
+  TCPPacket *recvPacket();
+  std::vector<TCPPacket *> readAndCreateTCPPackets(); // -> return vector<TCPPacket*> of the new packets created
   // potential sub function: createTCPPackets(vector<char> &, int startIndex, int endIndex) that creates TCP Packets from the byte buffer
   //unlike in the server, since there is only one connection at a given time, we can ensure that each function has complete autonomy over the that connection state
-  void handshake();  // hi!
-  void handwave();   // bye!
+  void handshake();       // hi!
+  void handwave();        // bye!
   void closeConnection(); // should handle both cases where server or client needs to do FIN
   // close connection should not be called by client until all packets are not ack'ed
   int congestionControl(); // change by 1 ACK, return the amount the CWND shifted
-  int shiftWindow(); // returns the number of bytes that the window has shifted
-  void markAck(TCPPacket* p);
-  
+  int shiftWindow();       // returns the number of bytes that the window has shifted
+  void markAck(TCPPacket *p);
+  int sendPacket(TCPPacket *p);
   /*
    * 
     resizeWindow
   */
+  bool isDup(TCPPacket *p);
 
-  
 private:
   int m_fileFd;
   int m_sockFd;
   int m_cwnd;
   int m_ssthresh;
   struct addrinfo m_serverInfo; // needed since we use sendto() and might have to provide server info each time
+  socklen_t m_serverInfoLen;
   ConnectionState m_state;
   bool m_fileRead; // file has been completely read and the winodw can't move any forward; can be a local variable in handleConnection() also
-  std::vector<char> m_buffer;
-  std::vector<TCPPacket*> m_packetBuffer;
+  std::vector<TCPPacket *> m_packetBuffer;
   std::vector<bool> m_packetACK;
   std::vector<c_time> m_packetTimers;
   std::vector<bool> m_sentOnce;
   int m_lseek;
+  int m_sequenceNumber;
+  int m_ackNumber;
+  int m_connectionId;
+  c_time connectionTimer();
+  int m_largestSeqNum;
+  int m_relSeqNum;
+
+  // private function
+  bool verifySynAck(TCPPacket *synAckPacket); // verifies the syn-ack packet of server
+  bool verifyFinAck(TCPPacket *finAckPacket); // verifies fin-ack packet of server
 };
 
 #endif
