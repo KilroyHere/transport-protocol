24a07c4d381d7ab1bf23c5ebbea1eaf891e9f979
Sanchit Agarwal <sanchit1591@g.ucla.edu>
2022-03-04 00:33:07 -0800

    minor fix

--- client.hpp
+++ client.hpp
@@ -58,7 +58,7 @@ private:
   std::vector<bool> m_packetACK;
   std::vector<c_time> m_packetTimers;
   std::vector<bool> m_sentOnce;
-  int m_lseek;
+
   int m_sequenceNumber;
   int m_ackNumber;
   int m_connectionId;
